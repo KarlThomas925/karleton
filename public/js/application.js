4303b9feee0f828897ca5440b5d817f2d357e1f4
Karl Thomas <email": null>
2017-03-21 15:50:37 -0500

    make new javascript form scraper

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
 
 });
+
+
+
+
+
+
+function formScrape (formObject){
+  if (formObject.find('input').attr('name') === "_method"){
+    var realMethod = formObject.find('input').attr('value');
+  } else {
+    var realMethod = formObject.attr('method');
+  }
+  var scrape = {
+    url: formObject.attr('action'),
+    method: realMethod,
+    data: formObject.serialize()
+  };
+  return scrape
+}
