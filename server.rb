30fb6f676e0a9ab1d4c64bb1cd6fd1828bd30bd8
karl-thomas <karl.thomas925@gmail.com>
2017-07-23 18:12:01 -0500

    now sends a basic http protocol, now for routes

--- server.rb
+++ server.rb
@@ -1,18 +1,23 @@
 require 'socket'
-
-server = TCPServer.new 2000 # Server bind to port 2000
+server = TCPServer.new("127.0.0.1", 2000) # Server bind to port 2000
 
 loop do
   client = server.accept    # Wait for a client to connect
-  input = client.gets.chomp
-  case input
-  when "home"
-    client.puts "you home now\n"
-  when "profile"
-    client.puts "name: Karl\n info: he does things \n"
-  else
-    client.puts "what \n"
-  end
-
+  response = ["HTTP/1.1 200 OK\r\n"]
+  response << "Server: Darwin\r\n"
+  response << "Content-type : text/html\r\n"
+  response << "Content-length: #{IO.binread('testfile.txt').length}\r\n"
+  response << "Connection: close\r\n"
+  response << "\r\n"
+  response << IO.binread('testfile.txt')
+  puts response.join
+  client.puts response.join
   client.close
 end
+
+  # n_r = "Server: Apache/2.4.1\n"
+  # n_r << "Content-type: text/html\n"
+  # n_r << "Content-length: #{IO.binread('testfile.txt').length}\n"
+  # n_r << "Connection: close\n"
+  # n_r << "\n"
+  # n_r << IO.binread('testfile.txt')
