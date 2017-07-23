b055b47d80abccc39cccf797a482a82c07b1099f
karl-thomas <karl.thomas925@gmail.com>
2017-07-23 02:29:38 -0500

    basic stdin/ stdout practice with IO methods

--- server.rb
+++ server.rb
@@ -4,6 +4,15 @@ server = TCPServer.new 2000 # Server bind to port 2000
 
 loop do
   client = server.accept    # Wait for a client to connect
-  client.puts "Hello karl"
+  input = client.gets.chomp
+  case input
+  when "home"
+    client.puts "you home now\n"
+  when "profile"
+    client.puts "name: Karl\n info: he does things \n"
+  else
+    client.puts "what \n"
+  end
+
   client.close
 end
