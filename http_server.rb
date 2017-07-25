021f8d83f25fee22061ee37cdf880e7552800cf6
karl-thomas <karl.thomas925@gmail.com>
2017-07-25 18:04:50 -0500

    refactors sending a response usings a contruction method instead of pushing strings into an instance variable

--- http_server.rb
+++ http_server.rb
@@ -7,34 +7,31 @@ class HTTPServer
   def initialize(host, port)
     @server = TCPServer.new(host, port)
     @status = "200 OK"
-    @server_response = []
     @resource = IO.binread('welcome.txt')
     @client_uri = nil
-  end
+    @icon = "<link rel='icon' type='image/png' href='bear-face-icon.png' />"  
+end
 
   def run
     loop do
       client = self.server.accept    # Wait for a client to connect
-      client.puts client.methods.sort
-      client.puts "###****** _|____|_  ``(._.``)   ******####"
       resource_request = parse_uri(client)
-      client.puts resource_request
       client.puts send_resource_for(resource_request)
       client.close
     end
   end
 
   def send_resource_for(resource_request)
-    find_resource(resource_request)
-    set_status_line
-    set_headers
-    add_resource
-    self.server_response.join
+    resource = find_resource(resource_request)
+    status_line = set_status_line
+
+    construct_response(status_line, response_headers(resource), resource)
   end
 
   def find_resource(resource_name)
     return @resource if resource_name == ""
-    
+    return @icon if resource_name == "favicon.ico"
+
     if read_file = IO.binread("#{resource_name}.txt")
       @resource = read_file
     else
@@ -42,31 +39,40 @@ class HTTPServer
     end
   end
 
+  def construct_response(status_line, headers, resource)
+    response = []
+    response << status_line
+    response << headers 
+    response << resource
+    response.join
+  end
+
   def set_status_line
-    self.server_response << "HTTP/1.1 #{self.status}\r\n"
+    "HTTP/1.1 #{self.status}\r\n"
   end
 
-  def set_headers
-    add_header("Server" => "Darwin")
-    add_header("Content-type" => "text/html")
-    add_header("Content-length" => @resource.length)
-    add_header("Connection" => "close")
+  def response_headers(resource)
+    headers = [ header("Server" => "Darwin"),
+                header("Content-type" => "text/html"),
+                header("Content-length" => resource.length),
+                header("Connection" => "close") ]
   end
 
-  def add_header(arg = {})
+
+  def header(arg = {})
     header = arg.keys[0]
     value = arg[header]
-    self.server_response << "#{header}: #{value} \r\n"
+    "#{header}: #{value} \r\n"
   end
 
   def add_resource
-    self.server_response << "\r\n"
-    self.server_response << @resource
+    "\r\n" + @resource
   end
 
   def parse_uri(client_request)
     uri = client_request.first.split[1]
     uri[0] = ""
+
     self.client_uri = uri
   end 
 
