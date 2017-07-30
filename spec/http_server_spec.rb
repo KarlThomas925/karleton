b9cd5b449a5a3a96e9c10495736b082c2dadfb2a
karl-thomas <karl.thomas925@gmail.com>
2017-07-29 19:06:21 -0500

    adds testing for header_to_s

--- spec/http_server_spec.rb
+++ spec/http_server_spec.rb
@@ -4,31 +4,37 @@ require_relative "../http_response"
 describe HTTPServer do
   let(:host) { "127.0.0.1" }
   let(:port) { 2000 }
-
-  describe "On initialization" do
   
-    before(:each) do 
-      @server = HTTPServer.new(host, port) 
-    end
-
-    after(:each) do
-      @server.tcp_server.close
-    end
-
+  around(:each) do |example|
+    @server = HTTPServer.new(host, port) 
+    example.run
+    @server.tcp_server.close
+  end
+  
+  describe "On initialization" do
     it "has a readable instance of TCPServer assigned to it" do 
-      expect(@server.tcp_server).to be_an_instance_of(TCPServer)
+      expect( @server.tcp_server ).to be_an_instance_of(TCPServer)
     end
 
     it "does NOT have a writable instance of TCPServer" do
-      expect{@server.tcp_server}.to throw
+      expect{ @server.tcp_server = "paul" }.to raise_error(NoMethodError)
     end
 
     it "has a readable instance of HTTPResponse to respond with." do 
-      expect(@server.server_response).to be_an_instance_of(HTTPResponse)
+      expect( @server.server_response ).to be_an_instance_of(HTTPResponse)
     end
 
     it "does NOT have a writable instance of HTTPResponse" do
-      
+       expect{ @server.server_response = "qymana" }.to raise_error(NoMethodError)
     end
-  end  
+  end 
+
+  describe "header_to_s" do
+    it "formats a key value pair in HTTP response header format" do
+      correctly_formated_header = "Header: value \r\n"
+      expect(@server.header_to_s(Header: "value")).to eq correctly_formated_header
+    end
+  end
+
 end
+
